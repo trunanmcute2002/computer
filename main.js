d73217c836ec52c912bac92c216ad53e7845d306
TrungNam <167555150+trunanmcute2002@users.noreply.github.com>
2024-06-24 20:32:23 +0700

    up load computer

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let name="Trung Nam"
+
+
+function tuoi(){
+    if(tuoi % 2==0){
+        document.write(tuoi + "số lẻ")
+    }
+    else{
+        document.write(tuoi + "số chẵn")
+    }
+    
+}
+tuoi=document.write(prompt("Nhập tuoi"));
